e11f9b4f0a139b1e8e7f80d5e4e7699017bc1898
amirazman73 <amirazman73@gmail.com>
2023-11-07 16:40:41 +0100

    update blocks

--- week_3/blocks.py
+++ week_3/blocks.py
@@ -4,6 +4,11 @@ import numpy as np
 # put `w2_sigmoid_forward` and `w2_sigmoid_grad_input` here #
 #######################################################
 
+def w2_sigmoid_forward(x_input):
+    return 1 / (1 + np.exp(-x_input))
+
+def w2_sigmoid_grad_input(x_input, grad_output):
+    return grad_output * (1-grad_output)
 
 #######################################################
 # put `w2_nll_forward` and `w2_nll_grad_input` here    #
