c33d85dca866ba1b1b6763f444fd0474d228b6a1
amirazman73 <amirazman73@gmail.com>
2023-11-19 22:57:39 +0100

    updates to run tests

--- week_3/blocks.py
+++ week_3/blocks.py
@@ -5,11 +5,66 @@ import numpy as np
 #######################################################
 
 def w2_sigmoid_forward(x_input):
-    return 1 / (1 + np.exp(-x_input))
+    """sigmoid nonlinearity
+    # Arguments
+        x_input: np.array of size `(n_objects, n_in)`
+    # Output
+        the output of sigmoid layer
+        np.array of size `(n_objects, n_in)`
+    """
+    output = 1 / (1 + np.exp(-x_input))
+
+    return output
 
 def w2_sigmoid_grad_input(x_input, grad_output):
-    return grad_output * (1-grad_output)
+    """sigmoid nonlinearity gradient. 
+        Calculate the partial derivative of the loss 
+        with respect to the input of the layer
+    # Arguments
+        x_input: np.array of size `(n_objects, n_in)`
+        grad_output: np.array of size `(n_objects, n_in)` 
+            dL / df
+    # Output
+        the partial derivative of the loss 
+        with respect to the input of the function
+        np.array of size `(n_objects, n_in)` 
+        dL / dh
+    """
+    sig = 1 / (1 + np.exp(-x_input))
+    dev_sig = sig * (1-sig)
+    grad_input = grad_output*dev_sig
+
+    return grad_input
 
 #######################################################
 # put `w2_nll_forward` and `w2_nll_grad_input` here    #
 #######################################################
+
+def w2_nll_grad_input(target_pred, target_true):
+    """Compute the partial derivative of NLL
+        with respect to its input
+    # Arguments
+        target_pred: predictions - np.array of size `(n_objects, 1)`
+        target_true: ground truth - np.array of size `(n_objects, 1)`
+    # Output
+        the partial derivative 
+        of NLL with respect to its input
+        np.array of size `(n_objects, 1)`
+    """
+    grad_input = ((target_pred - target_true)/ (target_pred * (1 - target_pred))) / len(target_pred)
+    return grad_input
+
+
+def w2_nll_forward(target_pred, target_true):
+    """Compute the value of NLL
+        for a given prediction and the ground truth
+    # Arguments
+        target_pred: predictions - np.array of size `(n_objects, 1)`
+        target_true: ground truth - np.array of size `(n_objects, 1)`
+    # Output
+        the value of NLL for a given prediction and the ground truth
+        scalar
+    """
+    output = -np.sum(target_true * np.log(target_pred) + (1 - target_true)* np.log(1-target_pred)) / len(target_pred)
+    
+    return output
